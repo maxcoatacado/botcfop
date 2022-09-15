6c6c6c23f0ed22327f54757429f76cec5b95e94d
nielitonmaxco <nielitonmaxco@gmail.com>
2022-09-15 17:26:41 -0300

    fixed navigator load

--- blingCte.py
+++ blingCte.py
@@ -1,3 +1,4 @@
+from time import sleep
 from selenium import webdriver
 import os
 from selenium.webdriver.common.by import By
@@ -9,10 +10,12 @@ chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 navigator = webdriver.Chrome(os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
 navigator.get('https://www.bling.com.br/ctes.php#list')
+sleep(5)
+
 
 email = navigator.find_element(By.CSS_SELECTOR, '#username')
+email.send_keys('nieliton@maxcoatacado.com.br')
 print(email)
-# email.send_keys('nieliton@maxcoatacado.com.br')
 
 
 # sleep(5)
