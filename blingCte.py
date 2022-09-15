86e4968b876c8c5909eb967598df09f1fe1a5ab1
nielitonmaxco <nielitonmaxco@gmail.com>
2022-09-15 17:03:57 -0300

    fixed os

--- blingCte.py
+++ blingCte.py
@@ -6,7 +6,7 @@ chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
-navigator = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+navigator = webdriver.Chrome(os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
 navigator.get('https://www.bling.com.br/ctes.php#list')
 print(navigator.title)
 
